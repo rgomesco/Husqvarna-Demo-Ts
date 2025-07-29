23ae1f49aaa7f708a3abc166038894158ccd5843
rgomesco <rudolfcoutinho@gmail.com>
2025-07-30 01:14:05 +0530

    added config for video on fail

--- .testcaferc.js
+++ .testcaferc.js
@@ -16,7 +16,13 @@ module.exports = {
     screenshots: {
         takeOnFails: true,
         path: "./reports/screenshots",
-        pathPattern: "${DATE}_${TIME}/{USERAGENT}/${TEST}/${FILE_INDEX}.png"
+        pathPattern: "${DATE}_${TIME}/${USERAGENT}/${TEST}/${FILE_INDEX}.png"
+    },
+    videoPath: "./reports/videos",
+    videoOptions: {
+        singleFile: false,
+        failedOnly: true,
+        pathPattern: "${DATE}_${TIME}/${USERAGENT}/${TEST}.mp4"
     },
     hooks: {
         test: {
